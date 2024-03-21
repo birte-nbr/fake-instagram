2c408321ff0afa8bb5a63b872cc2c6b6103614cc
birte-nbr <birteneubauer15@gmail.com>
2024-03-21 13:13:18 +0000

    added fileupload

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require('express'); //required to use 'express' module that allows to template our pages
 const path = require("path"); //required to use 'path' module that gets the current directory
 const app = express(); //create an app from express
-const port = 3726; 
+const port = 3800; 
 
 // Set up the 'views' directory for EJS templates
 app.set('views', path.join(__dirname, 'views'));
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
+
+//------------------------File upload---------------------------
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
 
 
 // Define a basic route for the root URL ("/")
@@ -86,7 +90,7 @@ app.get("/users/:username", (req, res) => {
 }); */
 
 app.get("/testmysql/", (req, res) => {
-    const query = "SELECT * FROM `host_summary`";
+    const query = "SELECT * FROM `users`";
     console.log(query);
     connection.query(query, function(error, result, _fields) {
         if (error != null) {
@@ -101,4 +105,41 @@ app.get("/testmysql/", (req, res) => {
         });
     });
 });
+
+// Define a route for a specific URL ("/uploadpage") which renders the upload-image
+app.get("/uploadpage", (req, res) => {
+    return res.render("upload-image");
+});
+
+
+
+// Route to handle the form submission
+// Route to handle the form submission
+app.post('/upload', async(req, res) => {
+    console.log("uploading image");
+    // Access form data, including the image and caption
+    const caption = req.body.caption;
+    const uploadedFile = req.files.image;
+    //const query = "INSERT INTO `users` "
+
+
+    var newfilename = "21.03.2024-" + uploadedFile.name;
+
+    // Move the uploaded image to a specified folder
+    const uploadPath = path.join(__dirname, 'assets/uploads', newfilename);
+    uploadedFile.mv(uploadPath, (err) => {
+        if (err) { //debug error if occurs
+            return res.status(500).send(err);
+        }
+// Process the file as needed (e.g., save to database, send a response, etc.)
+        // ...
+        console.log("uploaded to" + uploadPath);
+        res.send('File uploaded!');
+    });
+
+    // Send a response
+    console.log('Form submitted successfully!');
+});
+
+
 
