93439c28100a0e2311c9b25efd46c98db0156eb0
birte-nbr <birteneubauer15@gmail.com>
2024-04-21 23:09:38 +0100

    added profile

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const express = require('express'); //required to use 'express' module that allo
 const path = require("path"); //required to use 'path' module that gets the current directory
 const app = express(); //create an app from express
 const port = process.env.PORT; //define our port number, this doesn’t have to be 3000
-const session = require('express-session');
+//const session = require('express-session');
 const users = require('./routes/UserRoutes'); // importing all user routes
 const posts = require('./routes/PostsRoutes'); // importing all post routes
 const login = require('./routes/LoginRoutes');
-
+//const MySQLStore = require("express-mysql-session")(session);
+/*
+// set up session store for logging in users
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  clearExpired: true,
+  checkExpirationInterval: 900000,
+  expiration: 86400000,
+});
+*/
 
 //------------------------File upload---------------------------
 const fileUpload = require("express-fileupload");
@@ -27,55 +40,23 @@ app.use(users);  // setting all paths
 app.use(posts);
 app.use(login);
 
-app.use(session({
-  secret: 'schloss-einstein', 
-  resave: false,  
-  saveUninitialized: false  
-}))
+
 // Start the server
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 
-
-// route where users can upload files
 /*
-app.get("/uploadpage", (req, res) => {
-    let chooseImage = true;
-    return res.render("upload-image", { chooseImage });
-});*/
-
-/// Route to handle the form submission
-/*
-app.post('/upload', async(req, res) => {
-    console.log("uploading image");
-    // Access form data, including the image, caption, alt-text etc
-    const caption = req.body.caption;
-    
-    const author = req.body.author;
-    const altText = req.body.altText;
-    
-    
-
-    // Move the uploaded image to a specified folder
-    
-    
-
-        console.log("uploaded to" + uploadPath);
-        res.send('File uploaded!');
-        const query = "INSERT INTO photos (author_id, date, path, caption, alt_text) VALUES (?, NOW(), ?, ?, ?)";
-        
-        connection.query(query, [author, newfilename, caption, altText], (error, results, fields) => {
-            if (error) {
-              console.error('Error inserting into database:', error);
-              return;
-            }
-            
-            console.log('Inserted into database successfully.');
-          });
-    });
-
-    // Send a response
-    console.log('Form submitted successfully!');
+sessionStore
+  .onReady()
+  .then(() => {
+    // MySQL session store ready for use.
+    console.log("MySQLStore ready");
+  })
+  .catch((error) => {
+    // Something went wrong.
+    console.error("MySQLStore error", error);
+  });
 
 */
+
 
